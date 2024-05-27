e6866b9cacf11fca58834b84157b80eab58d3f07
gaolei <gurecn@163.com>
2024-05-27 18:17:56 +0800

    fix(skb):优化设置界面菜单效果

--- src/main/java/com/yuyan/imemodule/constant/CustomConstant.kt
+++ src/main/java/com/yuyan/imemodule/constant/CustomConstant.kt
@@ -25,5 +25,4 @@ object CustomConstant {
     const val githubRepo = "https://github.com/gurecn/YuyanIme"
     const val licenseSpdxId = "LGPL-2.1-or-later"
     const val licenseUrl = "https://www.gnu.org/licenses/old-licenses/lgpl-2.1"
-    const val privacyPolicyUrl = "https://fcitx5-android.github.io/privacy/"
 }
