c8af208a33d9c73883378baab43fcdfceb0733e3
Gaolei <gurecn@gmail.com>
2024-08-20 10:12:30 +0800

    fix(ime):完善输入法方案命名

--- src/main/java/com/yuyan/imemodule/constant/CustomConstant.kt
+++ src/main/java/com/yuyan/imemodule/constant/CustomConstant.kt
@@ -16,15 +16,15 @@ object CustomConstant {
     const val EMOJI_TYPR_FACE_DATA = 4 //emoji表情：4。   序号需和SymbolsManager中mSymbolsEmoji Key对应
     const val EMOJI_TYPR_SMILE_TEXT = 5 //颜文字：5。   序号需和SymbolsManager中mSymbolsEmoji Key对应
     //按键长按选择
-    const val SCHEMA_ZH_T9 = "t9" // 拼音九键
-    const val SCHEMA_ZH_QWERTY = "rime_ice" // 拼音全键
-    const val SCHEMA_EN = "melt_eng" // 英文九键
+    const val SCHEMA_ZH_T9 = "pinyin_t9" // 拼音九键
+    const val SCHEMA_ZH_QWERTY = "pinyin" // 拼音全键
+    const val SCHEMA_EN = "english"         // 英语方案
     const val SCHEMA_ZH_HANDWRITING = "handwriting" // 手写输入
-    const val SCHEMA_ZH_DOUBLE_FLYPY = "double_pinyin_flypy" // 明月拼音+小鹤双拼
-    const val SCHEMA_ZH_DOUBLE_LX17 = "double_pinyin_ls17" // 乱序17
+    const val SCHEMA_ZH_DOUBLE_FLYPY = "double_pinyin_flypy" // 小鹤双拼
+    const val SCHEMA_ZH_DOUBLE_LX17 = "double_pinyin_ls17" // 乱序17双拼
     const val CURRENT_RIME_DICT_DATA_VERSIOM = 20240819
     const val YuyanIMERepo = "https://github.com/gurecn/YuyanIme"
-    const val YuyanSDKRepo = "https://github.com/gurecn/YuyanIme"
+    const val YuyanSDKRepo = "https://github.com/gurecn/yuyansdk"
     const val licenseSpdxId = "LGPL-2.1-or-later"
     const val licenseUrl = "https://www.gnu.org/licenses/old-licenses/lgpl-2.1"
 }
