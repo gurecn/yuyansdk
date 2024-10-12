8ff8501de75d6ab8af0bd76fa997870e127d3666
fugl <gurecn@gmail.com>
2024-10-12 15:14:35 +0800

    fix(skb):添加侧边栏操作提示

--- src/main/java/com/yuyan/imemodule/ui/fragment/PrefixSettingsFragment.kt
+++ src/main/java/com/yuyan/imemodule/ui/fragment/PrefixSettingsFragment.kt
@@ -20,7 +20,18 @@ import com.yuyan.imemodule.constant.CustomConstant
 import com.yuyan.imemodule.prefs.AppPrefs
 import com.yuyan.imemodule.view.keyboard.KeyboardManager
 import splitties.dimensions.dp
+import splitties.views.dsl.constraintlayout.below
+import splitties.views.dsl.constraintlayout.bottomOfParent
+import splitties.views.dsl.constraintlayout.centerHorizontally
+import splitties.views.dsl.constraintlayout.constraintLayout
+import splitties.views.dsl.constraintlayout.lParams
+import splitties.views.dsl.constraintlayout.startOfParent
+import splitties.views.dsl.constraintlayout.topOfParent
+import splitties.views.dsl.core.add
 import splitties.views.dsl.core.editText
+import splitties.views.dsl.core.matchParent
+import splitties.views.dsl.core.textView
+import splitties.views.dsl.core.wrapContent
 
 class PrefixSettingsFragment(pos:Int) : Fragment(){
     private var  positon = 0
@@ -34,10 +45,10 @@ class PrefixSettingsFragment(pos:Int) : Fragment(){
 
         var oldText = ""
         editText = editText {
-            val spannableString = SpannableString((if(positon == 0)CustomConstant.PREFIXS_PINYIN else CustomConstant.PREFIXS_NUMBER).joinToString ("\n"))
-            spannableString.setSpan(UnderlineSpan(), 0, spannableString.length, Spanned.SPAN_INCLUSIVE_EXCLUSIVE)
-            spannableString.setSpan( LineHeightSpanCustom(dp(5)), 0, spannableString.length, Spanned.SPAN_INCLUSIVE_EXCLUSIVE)
-            setText(spannableString)
+            setText(SpannableString((if(positon == 0)CustomConstant.PREFIXS_PINYIN else CustomConstant.PREFIXS_NUMBER).joinToString ("\n")).apply {
+                setSpan(UnderlineSpan(), 0, this.length, Spanned.SPAN_INCLUSIVE_EXCLUSIVE)
+                setSpan( LineHeightSpanCustom(dp(5)), 0, this.length, Spanned.SPAN_INCLUSIVE_EXCLUSIVE)
+            })
             requestFocus()
             background = null
             gravity = Gravity.CENTER_HORIZONTAL
@@ -62,20 +73,40 @@ class PrefixSettingsFragment(pos:Int) : Fragment(){
                 }
                 val content = prefixs.joinToString ("\n")
                 if(onChanged){
-                    this@editText.setText(content)
+                    setText(SpannableString(content).apply {
+                        setSpan(UnderlineSpan(), 0, this.length, Spanned.SPAN_INCLUSIVE_EXCLUSIVE)
+                        setSpan( LineHeightSpanCustom(dp(5)), 0, this.length, Spanned.SPAN_INCLUSIVE_EXCLUSIVE)
+                    })
                     this@editText.setSelection(if(start > content.length)content.length else start)
                 }
                 onWaitSave = true
             }
         }
-        editText
+
+        val tips = textView {
+            text = "最大支持15行，每行支持4个字符"
+            gravity = Gravity.CENTER
+        }
+
+        constraintLayout {
+            add(editText, lParams(height = wrapContent) {
+                centerHorizontally()
+                topOfParent()
+                startOfParent()
+            })
+            add(tips, lParams(width = matchParent, height = dp(20)) {
+                below(editText)
+                centerHorizontally()
+                bottomOfParent()
+            })
+        }
     }
 
     override fun onStop() {
         super.onStop()
         if(onWaitSave) {
             val prefixs = editText.text.toString().split(Regex("\n")).filterNot { it.isEmpty() }
-            val prefix = prefixs.joinToString ("\n")
+            val prefix = prefixs.joinToString (" ")
             if (positon == 0) {
                 AppPrefs.getInstance().internal.keyboardPrefixsPinyin.setValue(prefix)
                 CustomConstant.PREFIXS_PINYIN = prefixs.toTypedArray()
