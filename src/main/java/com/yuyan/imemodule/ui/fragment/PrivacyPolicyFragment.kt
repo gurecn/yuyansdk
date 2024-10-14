8b254dc738cc2a7cb5a57a9edbae1f27c6f7cded
fugl <gurecn@gmail.com>
2024-10-14 17:46:22 +0800

    fix(app):优化隐私设置

--- src/main/java/com/yuyan/imemodule/ui/fragment/PrivacyPolicyFragment.kt
+++ src/main/java/com/yuyan/imemodule/ui/fragment/PrivacyPolicyFragment.kt
@@ -1,26 +1,29 @@
 
 package com.yuyan.imemodule.ui.fragment
 
-import android.app.AlertDialog
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import androidx.lifecycle.lifecycleScope
 import androidx.preference.PreferenceFragmentCompat
 import com.yuyan.imemodule.R
+import com.yuyan.imemodule.entity.PrivacyPolicy
 import com.yuyan.imemodule.ui.utils.addPreference
-import com.mikepenz.aboutlibraries.entity.License
 import kotlinx.coroutines.launch
+import kotlinx.serialization.json.Json
 
 class PrivacyPolicyFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         lifecycleScope.launch {
             preferenceScreen = preferenceManager.createPreferenceScreen(requireContext()).apply {
-                val jsonString = resources.getString(R.string.privacy_policy_content)
-                addPreference(
-                    title = jsonString,
-                )
+                val jsonString = resources.openRawResource(R.raw.privacypolicy)
+                    .bufferedReader()
+                    .use { it.readText() }
+                Json.decodeFromString<List<PrivacyPolicy>>(jsonString).forEach {
+                    addPreference(
+                        title = it.name,
+                        summary = it.description
+                    )
+                }
             }
         }
     }
