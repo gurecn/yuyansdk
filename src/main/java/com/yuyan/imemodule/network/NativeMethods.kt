2eab270120e8b4fa4e417e5b0288acf7155a63ef
fugl <gurecn@gmail.com>
2024-10-18 00:18:33 +0800

    fix(skb):优化手写识别速度

--- src/main/java/com/yuyan/imemodule/network/NativeMethods.kt
+++ src/main/java/com/yuyan/imemodule/network/NativeMethods.kt
@@ -2,6 +2,8 @@ package com.yuyan.imemodule.network
 
 class NativeMethods {
     external fun nativeHttpPost(jsonRequest: String?): String?
+    external fun nativeHttpInit()
+    external fun nativeHttpStop()
 
     companion object {
         init {
