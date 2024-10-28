f31059b8b8743e4ff48d1ca24b3b073833500f1c
fugl <gurecn@gmail.com>
2024-10-28 09:24:18 +0800

    fix(app):增加输入法离线版本

--- src/main/java/com/yuyan/imemodule/view/keyboard/container/SettingsContainer.kt
+++ src/main/java/com/yuyan/imemodule/view/keyboard/container/SettingsContainer.kt
@@ -7,6 +7,7 @@ import android.view.View
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.flexbox.FlexboxLayoutManager
+import com.yuyan.imemodule.BuildConfig
 import com.yuyan.imemodule.R
 import com.yuyan.imemodule.adapter.MenuAdapter
 import com.yuyan.imemodule.constant.CustomConstant
@@ -146,13 +147,15 @@ class SettingsContainer(context: Context, inputView: InputView) : BaseContainer(
                 SkbMenuMode.Pinyin26Jian
             )
         )
-        funItems.add(
-            SkbFunItem(
-                mContext.getString(R.string.keyboard_name_hand),
-                R.drawable.selece_input_mode_handwriting,
-                SkbMenuMode.PinyinHandWriting
+        if(!BuildConfig.offline) {
+            funItems.add(
+                SkbFunItem(
+                    mContext.getString(R.string.keyboard_name_hand),
+                    R.drawable.selece_input_mode_handwriting,
+                    SkbMenuMode.PinyinHandWriting
+                )
             )
-        )
+        }
         val doublePYSchemaMode = AppPrefs.getInstance().input.doublePYSchemaMode.getValue()
         val doublePinyinSchemaName = when (doublePYSchemaMode) {
             DoublePinyinSchemaMode.flypy -> R.string.double_pinyin_flypy_plus
