53465115b92ff1ee781519d802336f4176b1ec94
fugl <gurecn@gmail.com>
2024-11-05 21:22:35 +0800

    fix(app):优化隐私政策说明

--- src/main/java/com/yuyan/imemodule/ui/fragment/PrivacyPolicyFragment.kt
+++ src/main/java/com/yuyan/imemodule/ui/fragment/PrivacyPolicyFragment.kt
@@ -4,6 +4,7 @@ package com.yuyan.imemodule.ui.fragment
 import android.os.Bundle
 import androidx.lifecycle.lifecycleScope
 import androidx.preference.PreferenceFragmentCompat
+import com.yuyan.imemodule.BuildConfig
 import com.yuyan.imemodule.R
 import com.yuyan.imemodule.entity.PrivacyPolicy
 import com.yuyan.imemodule.ui.utils.addPreference
@@ -15,7 +16,7 @@ class PrivacyPolicyFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         lifecycleScope.launch {
             preferenceScreen = preferenceManager.createPreferenceScreen(requireContext()).apply {
-                val jsonString = resources.openRawResource(R.raw.privacypolicy)
+                val jsonString = resources.openRawResource(if(BuildConfig.offline)R.raw.privacypolicy else R.raw.privacypolicyonline)
                     .bufferedReader()
                     .use { it.readText() }
                 Json.decodeFromString<List<PrivacyPolicy>>(jsonString).forEach {
