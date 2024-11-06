7028e28edd5b8e1590154eebf219d7ec47ee158f
fugl <gurecn@gmail.com>
2024-11-06 08:44:17 +0800

    feature(skb):增加五笔画部分资源

--- src/main/java/com/yuyan/imemodule/constant/CustomConstant.kt
+++ src/main/java/com/yuyan/imemodule/constant/CustomConstant.kt
@@ -18,6 +18,7 @@ object CustomConstant {
     const val SCHEMA_ZH_HANDWRITING = "handwriting" // 手写输入
     const val SCHEMA_ZH_DOUBLE_FLYPY = "double_pinyin_" // 小鹤双拼
     const val SCHEMA_ZH_DOUBLE_LX17 = "double_pinyin_ls17" // 乱序17双拼
+    const val SCHEMA_ZH_STROKE = "pinyin_stroke" // 五笔画
     const val CURRENT_RIME_DICT_DATA_VERSIOM = 20241029
     const val YUYAN_IME_REPO = "https://github.com/gurecn/YuyanIme"
     const val YUYAN_SDK_REPO = "https://github.com/gurecn/yuyansdk"
