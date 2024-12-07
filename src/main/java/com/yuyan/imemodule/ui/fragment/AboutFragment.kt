f66cefb65c5aeb359f8d6092d69bd6d04e84f565
fugl <gurecn@gmail.com>
2024-12-07 23:09:07 +0800

    feature(setting):增加点击版本跳转github最新版

--- src/main/java/com/yuyan/imemodule/ui/fragment/AboutFragment.kt
+++ src/main/java/com/yuyan/imemodule/ui/fragment/AboutFragment.kt
@@ -32,7 +32,10 @@ class AboutFragment : PreferenceFragmentCompat() {
             }
             addCategory(R.string.app_version) {
                 isIconSpaceReserved = false
-                addPreference(R.string.version, BuildConfig.versionName)
+                addPreference(R.string.version, BuildConfig.versionName){
+                    val uri = Uri.parse("${CustomConstant.YUYAN_IME_REPO}/releases/latest")
+                    startActivity(Intent(Intent.ACTION_VIEW, uri))
+                }
                 addPreference(R.string.build_git_hash, BuildConfig.AppCommitHead) {
                     val commit = BuildConfig.AppCommitHead.substringBefore('-')
                     val uri = Uri.parse("${CustomConstant.YUYAN_IME_REPO}/commit/${commit}")
