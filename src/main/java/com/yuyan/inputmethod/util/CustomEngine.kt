90ed23d12cfeaf98046befc9b2f27dce3832fe70
fugl <gurecn@gmail.com>
2024-12-26 15:32:24 +0800

    fix(ime):优化常用语联想

---
+++ src/main/java/com/yuyan/inputmethod/util/CustomEngine.kt
@@ -0,0 +1,55 @@
+package com.yuyan.inputmethod.util
+
+import com.yuyan.imemodule.database.DataBaseKT
+import com.yuyan.imemodule.utils.StringUtils
+import com.yuyan.imemodule.utils.expression.ExpressionBuilder
+
+object CustomEngine {
+    fun parseExpressionAtEnd(input: String): String? {
+        val expressionEndPattern = "((φ|π|pi|sin|cos|tan|cot|asin|acos|atan|sinh|cosh|tanh|abs|log|log1p|ceil|floor|sqrt|cbrt|pow|exp|expm|signum|csc|sec|csch|sech|coth|toradian|todegree|\\(|\\)|^|%|\\+|-|\\*|/|\\.|e|E|\\d)+)$".toRegex()
+        return expressionEndPattern.find(input.removeSuffix("="))?.value
+    }
+
+    fun expressionCalculator(input: String, expression: String):Array<String>{
+        val results = mutableListOf<String>()
+        if(!StringUtils.isNumber(expression) && !StringUtils.isLetter(expression)){
+            try {
+                val evaluate = ExpressionBuilder(expression).build().evaluate()
+                val  resultFloat = evaluate.toFloat()
+                val  resultInt = evaluate.toInt()
+                if(evaluate.compareTo(resultInt) == 0){
+                    val resultIntStr = resultInt.toString()
+                    results.add(resultIntStr)
+                    if(!input.endsWith("=")) results.add("=".plus(resultIntStr))
+                } else {
+                    val resultFloatStr = resultFloat.toString()
+                    results.add(resultFloatStr)
+                    if(!input.endsWith("=")) results.add("=".plus(resultFloatStr))
+                    if(resultFloat < 1 && resultFloat > 0){
+                        results.add((evaluate * 100).toInt().toString() + "%")
+                    }
+                }
+            } catch (_:Exception){ }
+            results.addAll(arrayOf("=", "+", "-", "*", "/", "%", ".", ",", "'", "(", ")"))
+        }
+        return results.toTypedArray()
+    }
+
+    fun predictAssociationWordsChinese(text: String):MutableList<String> {
+        val associations = mutableListOf("，", "。")
+        val suffixesExclamation = setOf("啊", "呀", "呐", "啦", "噢", "哇", "吧", "呗", "了")
+        val suffixesQuestion = setOf("吗", "啊", "呢", "吧", "谁", "何", "什么", "哪", "几", "多少", "怎", "难道", "岂", "不")
+        if(suffixesExclamation.any{
+                text.endsWith(it)
+            }){ associations.add(0, "！") }
+        if(suffixesQuestion.any{
+                text.contains(it)
+            }){ associations.add(0, "？") }
+        return associations
+    }
+
+    fun processPhrase(text: String):List<String> {
+        val phrase = DataBaseKT.instance.phraseDao().query(text)
+        return phrase.map { it.content }
+    }
+}
