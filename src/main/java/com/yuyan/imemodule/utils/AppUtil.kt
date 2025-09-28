bf9d389c57951cd7e8e464ae98481165af42c710
gure <gurecn@163.com>
2025-09-29 01:52:58 +0800

    fix(ui):优化应用退出问题

--- src/main/java/com/yuyan/imemodule/utils/AppUtil.kt
+++ src/main/java/com/yuyan/imemodule/utils/AppUtil.kt
@@ -20,7 +20,7 @@ object AppUtil {
 
     fun launchSettings(context: Context) {
         context.startActivity<SettingsActivity> {
-            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
         }
     }
 
