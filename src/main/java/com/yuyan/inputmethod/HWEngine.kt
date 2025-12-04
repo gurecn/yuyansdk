9631bdfe447081900e42d93f6f7819399a5f3f9b
gure <gurecn@163.com>
2025-12-04 21:54:13 +0800

    fix(ime):优化手写识别模块

--- src/main/java/com/yuyan/inputmethod/HWEngine.kt
+++ src/main/java/com/yuyan/inputmethod/HWEngine.kt
@@ -7,14 +7,10 @@ import com.yuyan.imemodule.libs.pinyin4j.format.HanyuPinyinCaseType
 import com.yuyan.imemodule.libs.pinyin4j.format.HanyuPinyinOutputFormat
 import com.yuyan.imemodule.libs.pinyin4j.format.HanyuPinyinToneType
 import com.yuyan.imemodule.libs.pinyin4j.format.HanyuPinyinVCharType
-import com.yuyan.imemodule.utils.thread.ThreadPoolUtils
 import com.yuyan.inputmethod.core.CandidateListItem
 import com.yuyan.inputmethod.core.HandWriting
-import java.util.Collections
 
 object HWEngine {
-    private var isRecognitionState = false
-    private var  nextDatas  =  Collections.synchronizedList(mutableListOf<Pair<MutableList<Short?>, IHandWritingCallBack>?>())
     private var mHanyuPinyinOutputFormat: HanyuPinyinOutputFormat
 
     init {
@@ -27,50 +23,24 @@ object HWEngine {
         mHanyuPinyinOutputFormat.vCharType = HanyuPinyinVCharType.WITH_U_UNICODE
     }
 
-    fun getSettingOptions(): Array<String?> {
-        val arrayList: ArrayList<String> = ArrayList(10)
-        arrayList.add("Emoji")
-        arrayList.add("0")
-        arrayList.add("EnableFuzzy")
-        arrayList.add("0")
-        arrayList.add("Fuzzy")
-        arrayList.add("0")
-        arrayList.add("9KeyCorrect")
-        arrayList.add("0")
-        arrayList.add("26KeyCorrect")
-        arrayList.add("0")
-        return arrayList.toTypedArray() as Array<String?>
-    }
-
     fun recognitionData(strokes: MutableList<Short?>, recogResult: IHandWritingCallBack){
-        nextDatas.add(Pair(strokes, recogResult))
-        if(isRecognitionState) return
-        isRecognitionState = true
-        ThreadPoolUtils.Companion.executeSingleton {
-            while (true) {
-                if(nextDatas.isEmpty()) break
-                val data = nextDatas.removeAt(0)
-                nextDatas.clear()
-                HandWriting.reset()
-                val strokesData = data?.first?.toMutableList()
-                val intArray = strokesData!!.filterNotNull().map { it.toInt() }.toIntArray()
-                HandWriting.inputHWPoints(intArray)
-                val candidatesPyComposition = HandWriting.getCandidatesPyComposition()
-                val candidates = candidatesPyComposition[0]
-                if (candidates != null && candidates.isNotEmpty()) {
-                    val recogResultData = data.second
-                    val recogResultItems = ArrayList<CandidateListItem>()
-                    for (candidate in candidates) {
-                        recogResultItems.add(
-                            CandidateListItem(
-                                PinyinHelper.toHanYuPinyin(candidate!!, mHanyuPinyinOutputFormat, "'").ifEmpty { candidate }, candidate
-                            )
-                        )
-                    }
-                    recogResultData.onSucess(recogResultItems.toTypedArray())
-                }
+        HandWriting.reset()
+        val strokesData = strokes.toMutableList()
+        val intArray = strokesData.filterNotNull().map { it.toInt() }.toIntArray()
+        HandWriting.inputHWPoints(intArray)
+        val candidatesPyComposition = HandWriting.getCandidatesPyComposition()
+        val candidates = candidatesPyComposition[0]
+        if (candidates != null && candidates.isNotEmpty()) {
+            val recogResultData = recogResult
+            val recogResultItems = ArrayList<CandidateListItem>()
+            for (candidate in candidates) {
+                recogResultItems.add(
+                    CandidateListItem(
+                        PinyinHelper.toHanYuPinyin(candidate!!, mHanyuPinyinOutputFormat, "'").ifEmpty { candidate }, candidate
+                    )
+                )
             }
-            isRecognitionState = false
+            recogResultData.onSucess(recogResultItems.toTypedArray())
         }
     }
 }
