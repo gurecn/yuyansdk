a8cdeed81cc886624d083ffcf611318eeac73b3e
yumuing <yuzhe780@126.com>
2026-08-30 17:09:53 +0800

    feat(settings): add WebDAV sync config, interval and retention UI

--- src/main/java/com/yuyan/imemodule/ui/fragment/OtherSettingsFragment.kt
+++ src/main/java/com/yuyan/imemodule/ui/fragment/OtherSettingsFragment.kt
@@ -1,8 +1,11 @@
 package com.yuyan.imemodule.ui.fragment
 
 import android.content.ComponentName
+import android.content.Context
 import android.content.pm.PackageManager
 import android.net.Uri
+import android.text.InputType
+import android.widget.EditText
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -19,13 +22,19 @@ import com.yuyan.imemodule.R
 import com.yuyan.imemodule.application.Launcher
 import com.yuyan.imemodule.manager.UserDataManager
 import com.yuyan.imemodule.prefs.AppPrefs
+import com.yuyan.imemodule.rime.sync.RIME_SYNC_MODE_SAF
+import com.yuyan.imemodule.rime.sync.RIME_SYNC_MODE_WEBDAV
 import com.yuyan.imemodule.rime.sync.RimeSyncException
 import com.yuyan.imemodule.rime.sync.RimeSyncManager
 import com.yuyan.imemodule.rime.sync.RimeSyncStateStore
 import com.yuyan.imemodule.rime.sync.RimeSyncStorageBridge
+import com.yuyan.imemodule.rime.sync.RimeSyncScheduler
+import com.yuyan.imemodule.rime.sync.WebDavSyncConfig
+import com.yuyan.imemodule.rime.sync.WebDavSyncTransport
 import com.yuyan.imemodule.ui.activity.LauncherActivity
 import com.yuyan.imemodule.ui.fragment.base.ManagedPreferenceFragment
 import com.yuyan.imemodule.utils.AppUtil
+import com.yuyan.imemodule.utils.DevicesUtils
 import com.yuyan.imemodule.utils.addPreference
 import com.yuyan.imemodule.utils.importErrorDialog
 import com.yuyan.imemodule.utils.queryFileName
@@ -50,6 +59,14 @@ class OtherSettingsFragment: ManagedPreferenceFragment(AppPrefs.getInstance().ot
     private lateinit var syncDirLauncher: ActivityResultLauncher<Uri?>
     private var syncDirPreference: Preference? = null
     private var syncNowPreference: Preference? = null
+    private var syncClearPreference: Preference? = null
+    private var syncModePreference: Preference? = null
+    private var webDavUrlPreference: Preference? = null
+    private var webDavAccountPreference: Preference? = null
+    private var webDavPasswordPreference: Preference? = null
+    private var webDavTestPreference: Preference? = null
+    private var syncIntervalPreference: Preference? = null
+    private var retentionPreference: Preference? = null
 
     override fun onStart() {
         super.onStart()
@@ -126,12 +143,14 @@ class OtherSettingsFragment: ManagedPreferenceFragment(AppPrefs.getInstance().ot
         val ctx = requireContext()
         val rimeSyncCategory = PreferenceCategory(ctx).apply {
             title = getString(R.string.rime_sync_category)
+            isIconSpaceReserved = false
         }
         screen.addPreference(rimeSyncCategory)
 
         syncDirPreference = Preference(ctx).apply {
             title = getString(R.string.rime_sync_dir_pick)
             isSingleLineTitle = false
+            isIconSpaceReserved = false
             isCopyingEnabled = true
             setOnPreferenceClickListener {
                 AlertDialog.Builder(ctx)
@@ -150,6 +169,7 @@ class OtherSettingsFragment: ManagedPreferenceFragment(AppPrefs.getInstance().ot
         syncNowPreference = Preference(ctx).apply {
             title = getString(R.string.rime_sync_now)
             isSingleLineTitle = false
+            isIconSpaceReserved = false
             isCopyingEnabled = true
             setOnPreferenceClickListener {
                 lifecycleScope.withLoadingDialog(ctx, R.string.rime_sync_now) {
@@ -170,9 +190,10 @@ class OtherSettingsFragment: ManagedPreferenceFragment(AppPrefs.getInstance().ot
         }
         rimeSyncCategory.addPreference(syncNowPreference!!)
 
-        val syncClearPreference = Preference(ctx).apply {
+        syncClearPreference = Preference(ctx).apply {
             title = getString(R.string.rime_sync_clear)
             isSingleLineTitle = false
+            isIconSpaceReserved = false
             isCopyingEnabled = true
             setOnPreferenceClickListener {
                 AlertDialog.Builder(ctx)
@@ -191,7 +212,175 @@ class OtherSettingsFragment: ManagedPreferenceFragment(AppPrefs.getInstance().ot
                 true
             }
         }
-        rimeSyncCategory.addPreference(syncClearPreference)
+        rimeSyncCategory.addPreference(syncClearPreference!!)
+
+        syncModePreference = Preference(ctx).apply {
+            title = getString(R.string.rime_sync_mode)
+            isSingleLineTitle = false
+            isIconSpaceReserved = false
+            isCopyingEnabled = true
+            setOnPreferenceClickListener {
+                val modes = arrayOf(
+                    getString(R.string.rime_sync_mode_saf),
+                    getString(R.string.rime_sync_mode_webdav)
+                )
+                val store = RimeSyncStateStore(ctx)
+                val previousMode = store.loadOrCreate().syncMode
+                val currentWebDav = previousMode == RIME_SYNC_MODE_WEBDAV
+                AlertDialog.Builder(ctx)
+                    .setTitle(R.string.rime_sync_mode)
+                    .setSingleChoiceItems(modes, if (currentWebDav) 1 else 0) { _, which ->
+                        store.setSyncMode(
+                            if (which == 1) RIME_SYNC_MODE_WEBDAV else RIME_SYNC_MODE_SAF
+                        )
+                    }
+                    .setPositiveButton(android.R.string.ok) { _, _ ->
+                        rescheduleSync(ctx)
+                        refreshSyncPreferences()
+                    }
+                    .setNegativeButton(android.R.string.cancel) { _, _ ->
+                        store.setSyncMode(previousMode)
+                        rescheduleSync(ctx)
+                        refreshSyncPreferences()
+                    }
+                    .show()
+                true
+            }
+        }
+        rimeSyncCategory.addPreference(syncModePreference!!)
+
+        webDavUrlPreference = Preference(ctx).apply {
+            title = getString(R.string.rime_sync_webdav_url)
+            isSingleLineTitle = false
+            isIconSpaceReserved = false
+            isCopyingEnabled = true
+            setOnPreferenceClickListener {
+                showTextInputDialog(
+                    ctx,
+                    R.string.rime_sync_webdav_url,
+                    initial = RimeSyncStateStore(ctx).loadOrCreate().webDavUrl.orEmpty(),
+                    hint = getString(R.string.rime_sync_webdav_url_hint)
+                ) { text ->
+                    updateWebDavConfig(ctx, url = text)
+                }
+                true
+            }
+        }
+        rimeSyncCategory.addPreference(webDavUrlPreference!!)
+
+        webDavAccountPreference = Preference(ctx).apply {
+            title = getString(R.string.rime_sync_webdav_account)
+            isSingleLineTitle = false
+            isIconSpaceReserved = false
+            isCopyingEnabled = true
+            setOnPreferenceClickListener {
+                showTextInputDialog(
+                    ctx,
+                    R.string.rime_sync_webdav_account,
+                    initial = RimeSyncStateStore(ctx).loadOrCreate().webDavUsername.orEmpty()
+                ) { text ->
+                    updateWebDavConfig(ctx, username = text)
+                }
+                true
+            }
+        }
+        rimeSyncCategory.addPreference(webDavAccountPreference!!)
+
+        webDavPasswordPreference = Preference(ctx).apply {
+            title = getString(R.string.rime_sync_webdav_password)
+            isSingleLineTitle = false
+            isIconSpaceReserved = false
+            isCopyingEnabled = true
+            setOnPreferenceClickListener {
+                showTextInputDialog(
+                    ctx,
+                    R.string.rime_sync_webdav_password,
+                    initial = RimeSyncStateStore(ctx).loadOrCreate().webDavPassword.orEmpty(),
+                    isPassword = true
+                ) { text ->
+                    updateWebDavConfig(ctx, password = text)
+                }
+                true
+            }
+        }
+        rimeSyncCategory.addPreference(webDavPasswordPreference!!)
+
+        webDavTestPreference = Preference(ctx).apply {
+            title = getString(R.string.rime_sync_webdav_save_test)
+            isSingleLineTitle = false
+            isIconSpaceReserved = false
+            isCopyingEnabled = true
+            setOnPreferenceClickListener {
+                val config = WebDavSyncConfig.from(RimeSyncStateStore(ctx).loadOrCreate())
+                if (config == null) {
+                    Toast.makeText(ctx, R.string.rime_sync_webdav_incomplete, Toast.LENGTH_LONG).show()
+                } else {
+                    lifecycleScope.withLoadingDialog(ctx, R.string.rime_sync_webdav_save_test) {
+                        val result = WebDavSyncTransport(config).testConnection()
+                        result.onSuccess {
+                            Toast.makeText(ctx, R.string.rime_sync_webdav_test_ok, Toast.LENGTH_SHORT).show()
+                        }.onFailure { e ->
+                            Toast.makeText(
+                                ctx,
+                                ctx.getString(R.string.rime_sync_webdav_test_fail) + ": " + syncErrorMessage(e),
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+                }
+                true
+            }
+        }
+        rimeSyncCategory.addPreference(webDavTestPreference!!)
+
+        syncIntervalPreference = Preference(ctx).apply {
+            title = getString(R.string.rime_sync_interval)
+            isSingleLineTitle = false
+            isIconSpaceReserved = false
+            isCopyingEnabled = true
+            setOnPreferenceClickListener {
+                val labels = arrayOf(
+                    getString(R.string.rime_sync_interval_manual),
+                    getString(R.string.rime_sync_interval_6h),
+                    getString(R.string.rime_sync_interval_12h),
+                    getString(R.string.rime_sync_interval_24h)
+                )
+                val values = intArrayOf(0, 6, 12, 24)
+                AlertDialog.Builder(ctx)
+                    .setTitle(R.string.rime_sync_interval)
+                    .setItems(labels) { _, which ->
+                        RimeSyncStateStore(ctx).setSyncIntervalHours(values[which])
+                        rescheduleSync(ctx)
+                        refreshSyncPreferences()
+                    }
+                    .show()
+                true
+            }
+        }
+        rimeSyncCategory.addPreference(syncIntervalPreference!!)
+
+        retentionPreference = Preference(ctx).apply {
+            title = getString(R.string.rime_sync_retention)
+            isSingleLineTitle = false
+            isIconSpaceReserved = false
+            isCopyingEnabled = true
+            setOnPreferenceClickListener {
+                val values = intArrayOf(0, 7, 30, 90, 180)
+                val labels = values.map {
+                    if (it <= 0) getString(R.string.rime_sync_retention_off)
+                    else getString(R.string.rime_sync_retention_days, it)
+                }.toTypedArray()
+                AlertDialog.Builder(ctx)
+                    .setTitle(R.string.rime_sync_retention)
+                    .setItems(labels) { _, which ->
+                        RimeSyncStateStore(ctx).setRetentionDays(values[which])
+                        refreshSyncPreferences()
+                    }
+                    .show()
+                true
+            }
+        }
+        rimeSyncCategory.addPreference(retentionPreference!!)
 
         screen.addPreference(R.string.export_user_data) {
             lifecycleScope.launch {
@@ -218,6 +407,18 @@ class OtherSettingsFragment: ManagedPreferenceFragment(AppPrefs.getInstance().ot
         val ctx = requireContext()
         val state = RimeSyncStateStore(ctx).load()
         val treeUri = RimeSyncStateStore(ctx).getTreeUri()
+        val mode = state?.syncMode ?: RIME_SYNC_MODE_SAF
+        val isWebDav = mode == RIME_SYNC_MODE_WEBDAV
+        syncModePreference?.summary = ctx.getString(
+            if (isWebDav) R.string.rime_sync_mode_webdav else R.string.rime_sync_mode_saf
+        )
+        syncDirPreference?.isVisible = !isWebDav
+        syncClearPreference?.isVisible = !isWebDav
+        webDavUrlPreference?.isVisible = isWebDav
+        webDavAccountPreference?.isVisible = isWebDav
+        webDavPasswordPreference?.isVisible = isWebDav
+        webDavTestPreference?.isVisible = isWebDav
+        syncIntervalPreference?.isVisible = isWebDav
         syncDirPreference?.summary = when {
             treeUri == null -> ctx.getString(R.string.rime_sync_dir_not_set)
             !RimeSyncManager.hasValidTreePermission() ->
@@ -235,6 +436,82 @@ class OtherSettingsFragment: ManagedPreferenceFragment(AppPrefs.getInstance().ot
                     TimeUtils.iso8601UTCDateTime(state.lastSuccessTime)
                 )
         }
+        webDavUrlPreference?.summary = state?.webDavUrl?.takeIf(String::isNotEmpty)
+            ?: ctx.getString(R.string.rime_sync_webdav_not_set)
+        webDavAccountPreference?.summary = state?.webDavUsername?.takeIf(String::isNotEmpty)
+            ?: ctx.getString(R.string.rime_sync_webdav_not_set)
+        webDavPasswordPreference?.summary = if (state?.webDavPassword.isNullOrEmpty()) {
+            ctx.getString(R.string.rime_sync_webdav_password_unset)
+        } else {
+            ctx.getString(R.string.rime_sync_webdav_password_set)
+        }
+        syncIntervalPreference?.summary = intervalLabel(ctx, state?.syncIntervalHours ?: 0)
+        retentionPreference?.summary = retentionLabel(ctx, state?.retentionDays ?: 0)
+    }
+
+    private fun updateWebDavConfig(
+        ctx: Context,
+        url: String? = null,
+        username: String? = null,
+        password: String? = null
+    ) {
+        val store = RimeSyncStateStore(ctx)
+        val state = store.loadOrCreate()
+        store.setWebDavConfig(
+            url ?: state.webDavUrl,
+            username ?: state.webDavUsername,
+            password ?: state.webDavPassword
+        )
+        refreshSyncPreferences()
+    }
+
+    private fun rescheduleSync(ctx: Context) {
+        val state = RimeSyncStateStore(ctx).loadOrCreate()
+        val hours = if (state.syncMode == RIME_SYNC_MODE_WEBDAV) {
+            state.syncIntervalHours
+        } else {
+            0
+        }
+        RimeSyncScheduler.schedule(ctx, hours)
+    }
+
+    private fun intervalLabel(ctx: Context, hours: Int): String = when (hours) {
+        6 -> ctx.getString(R.string.rime_sync_interval_6h)
+        12 -> ctx.getString(R.string.rime_sync_interval_12h)
+        24 -> ctx.getString(R.string.rime_sync_interval_24h)
+        else -> ctx.getString(R.string.rime_sync_interval_manual)
+    }
+
+    private fun retentionLabel(ctx: Context, days: Int): String =
+        if (days <= 0) ctx.getString(R.string.rime_sync_retention_off)
+        else ctx.getString(R.string.rime_sync_retention_days, days)
+
+    private fun showTextInputDialog(
+        ctx: Context,
+        title: Int,
+        initial: String,
+        hint: String? = null,
+        isPassword: Boolean = false,
+        onInput: (String) -> Unit
+    ) {
+        val editText = EditText(ctx).apply {
+            setText(initial)
+            if (hint != null) this.hint = hint
+            inputType = if (isPassword) {
+                InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            } else {
+                InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_URI
+            }
+            setSelection(text.length)
+        }
+        val padding = DevicesUtils.dip2px(20)
+        editText.setPadding(padding, 0, padding, 0)
+        AlertDialog.Builder(ctx)
+            .setTitle(title)
+            .setView(editText)
+            .setPositiveButton(android.R.string.ok) { _, _ -> onInput(editText.text.toString()) }
+            .setNegativeButton(android.R.string.cancel, null)
+            .show()
     }
 
     /** UI 不解析 Exception.message 判断类型，只按异常类型分支。 */
@@ -253,6 +530,14 @@ class OtherSettingsFragment: ManagedPreferenceFragment(AppPrefs.getInstance().ot
             getString(R.string.rime_sync_read_failed)
         is RimeSyncException.ExternalWriteFailed ->
             getString(R.string.rime_sync_write_failed)
+        is RimeSyncException.WebDavNotConfigured ->
+            getString(R.string.rime_sync_webdav_incomplete)
+        is RimeSyncException.WebDavAuthFailed ->
+            getString(R.string.rime_sync_webdav_auth_failed)
+        is RimeSyncException.WebDavNetworkFailed ->
+            getString(R.string.rime_sync_webdav_network_failed)
+        is RimeSyncException.WebDavRemoteFailed ->
+            e.message ?: getString(R.string.rime_sync_webdav_remote_failed)
         else -> e.message ?: e.javaClass.simpleName
     }
 }
